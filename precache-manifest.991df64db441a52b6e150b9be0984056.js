35e22de02b5b36ca703e5e18f92411a20f9c80d4
Cameron Earl <cameron.earl@stgconsulting.com>
2019-08-04 00:46:59 -0700

    Updates

---
+++ precache-manifest.991df64db441a52b6e150b9be0984056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9faeeb239f14b99d8153a3f4c62df0b",
+    "url": "/nonogram/index.html"
+  },
+  {
+    "revision": "c202fe1955e6e2137741",
+    "url": "/nonogram/static/css/main.e0446239.chunk.css"
+  },
+  {
+    "revision": "a0d8fe8f4c73a3328aae",
+    "url": "/nonogram/static/js/2.abe20285.chunk.js"
+  },
+  {
+    "revision": "c202fe1955e6e2137741",
+    "url": "/nonogram/static/js/main.222297e4.chunk.js"
+  },
+  {
+    "revision": "c8f6558567c41835a659",
+    "url": "/nonogram/static/js/runtime~main.4c67096e.js"
+  }
+]);
