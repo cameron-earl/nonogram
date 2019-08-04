1b3721e3a467471f462053e815fc4c433cfb30e3
Cameron Earl <cameron.earl@stgconsulting.com>
2019-08-04 01:04:45 -0700

    Uncomment stored puzzles

--- src/resources/puzzles.ts
+++ src/resources/puzzles.ts
@@ -6,18 +6,18 @@ export type StoredPuzzle = {
 const p = (name: string, puzzle: string): StoredPuzzle => ({ name, puzzle });
 
 const borrowed: StoredPuzzle[] = [
-  // p('pi', 'ƀƼƑƤƊƄƄ'),
-  // p('house', 'ſƐƶƹƶƄƄ'),
-  // p('heart', 'ƀƮƾƶƃƄƄ'),
-  // p('arrow', 'ſƐƆƳƃƄƄ'),
-  // p('skull', 'ſƺƼƞƩƄƄ'),
-  // p('plane', 'ſƒƾƱƍƄƄ'),
-  // p('cross', 'ſƺƛƜƭƄƄ'),
-  // p('tower', 'ƀƖƺƤƍƄƄ'),
-  // p('hourglass', 'ƀƼƐƆƞƄƄ'),
-  // p('shuriken', 'ƀƎƱƦƸƄƄ'),
-  // p('leaf', 'ſƾƄƓƴƤƙƔƸƗƦƽƇƐƝƋſƉƉ'),
-  // p('music', 'ſƎƆƃƐưƸƃƃƐƶƎƻƾưƥſƉƉ'),
+  p('pi', 'ƀƼƑƤƊƄƄ'),
+  p('house', 'ſƐƶƹƶƄƄ'),
+  p('heart', 'ƀƮƾƶƃƄƄ'),
+  p('arrow', 'ſƐƆƳƃƄƄ'),
+  p('skull', 'ſƺƼƞƩƄƄ'),
+  p('plane', 'ſƒƾƱƍƄƄ'),
+  p('cross', 'ſƺƛƜƭƄƄ'),
+  p('tower', 'ƀƖƺƤƍƄƄ'),
+  p('hourglass', 'ƀƼƐƆƞƄƄ'),
+  p('shuriken', 'ƀƎƱƦƸƄƄ'),
+  p('leaf', 'ſƾƄƓƴƤƙƔƸƗƦƽƇƐƝƋſƉƉ'),
+  p('music', 'ſƎƆƃƐưƸƃƃƐƶƎƻƾưƥſƉƉ'),
   p('tapir', 'ƎƾƯſſƾſſƞƷſƎƽƗƂƸƽƀƼƮſƞƮƷƎƾƾƂƾƾƯƾƽƻƎƶƾƂƼƾƯƞƮƻƾƼƾƸƲƢƻƛƷƞƆƍƂưƲƟƻƛƷƎƦƮſƓƓ'),
 ];
 
